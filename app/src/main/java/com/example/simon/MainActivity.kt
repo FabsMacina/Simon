0e10aae6cb297fc856017fea1f8b4a57b1e2e3da
FabsMacina <fabioceretti04@gmail.com>
2026-04-10 17:18:37 +0200

    Initial commit

---
+++ app/src/main/java/com/example/simon/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.simon
+
+import androidx.activity.ComponentActivity
+import android.os.Bundle
+
+
+class MainActivity: ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+    }
+}
