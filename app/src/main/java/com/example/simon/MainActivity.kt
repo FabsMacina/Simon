6a2889171654600fc8caa189949c97ed17a14adb
FabsMacina <fabioceretti04@gmail.com>
2026-04-20 15:59:48 +0200

    added language button, fixed layout

--- app/src/main/java/com/example/simon/MainActivity.kt
+++ app/src/main/java/com/example/simon/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
@@ -22,6 +23,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.mutableStateOf
@@ -33,8 +37,10 @@ import androidx.compose.ui.unit.sp
 import com.example.simon.ui.theme.Blue
 import com.example.simon.ui.theme.Cyan
 import com.example.simon.ui.theme.Green
+import com.example.simon.ui.theme.Green2
 import com.example.simon.ui.theme.Magenta
 import com.example.simon.ui.theme.Red
+import com.example.simon.ui.theme.Red2
 import com.example.simon.ui.theme.Yellow
 import com.example.simon.ui.theme.SimonTheme
 
@@ -56,20 +62,41 @@ class MainActivity: ComponentActivity() {
 @Composable
 fun MainScreen(modifier: Modifier = Modifier){
     var buttonCounter by rememberSaveable { mutableStateOf("") }
+    val scrollState = rememberScrollState()
     ConstraintLayout(modifier = modifier) {
-        val (c1, c2, r1) = createRefs()
-        Column(modifier = Modifier.fillMaxHeight(0.6f).constrainAs(c1) {},
+        val (c1, c2, r1, r2) = createRefs()
+        Row(modifier = Modifier.fillMaxHeight(0.05f).constrainAs(r1) {
+            top.linkTo(parent.top)
+            bottom.linkTo(c1.top)
+            end.linkTo(parent.end)
+        }, horizontalArrangement = Arrangement.End){
+            Button(
+                modifier = Modifier.fillMaxHeight(),
+                shape = ButtonDefaults.shape,
+                contentPadding = PaddingValues(0.dp),
+                onClick = {}
+            ) {
+                Text(modifier = Modifier.padding(horizontal = 8.dp),
+                    text = "English",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Medium
+                )
+            }
+        }
+        Column(modifier = Modifier.fillMaxHeight(0.5f).constrainAs(c1) {
+            top.linkTo(r1.bottom, margin = 0.dp)
+        },
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.spacedBy(0.dp)) {
-            Row(modifier = Modifier.weight(1f), verticalAlignment = Alignment.CenterVertically) {
+            Row(modifier = Modifier.weight(1f).fillMaxHeight(), verticalAlignment = Alignment.CenterVertically) {
                 Button(
-                    modifier = modifier.weight(1f).fillMaxHeight(),
+                    modifier = modifier.weight(1f).fillMaxHeight().padding(top = 0.dp),
                     colors = ButtonDefaults.buttonColors(Red),
                     contentPadding = PaddingValues(0.dp),
                     onClick = { buttonCounter += "R, " }
                 ) {}
                 Button(
-                    modifier = modifier.weight(1f).fillMaxHeight(),
+                    modifier = modifier.weight(1f).fillMaxHeight().padding(top = 0.dp),
                     colors = ButtonDefaults.buttonColors(Green),
                     contentPadding = PaddingValues(0.dp),
                     onClick = { buttonCounter += "G, " }
@@ -109,26 +136,32 @@ fun MainScreen(modifier: Modifier = Modifier){
             }  ){
             Text(
                 text = buttonCounter,
-                modifier = modifier,
+                modifier = modifier.verticalScroll(scrollState),
                 fontSize = 20.sp,
                 fontWeight = FontWeight.Medium
             )
         }
-        Row(modifier = Modifier.padding(0.dp).fillMaxHeight(0.1f).constrainAs(r1){
+        Row(modifier = Modifier.padding(0.dp).fillMaxHeight(0.1f).constrainAs(r2){
             top.linkTo(c2.bottom)
         } ){
             Button(
                 modifier = Modifier.weight(1f).fillMaxHeight(),
-                colors = ButtonDefaults.buttonColors(Red),
+                colors = ButtonDefaults.buttonColors(Red2),
                 contentPadding = PaddingValues(0.dp),
                 onClick = {}
-            ) {}
+            ) {
+                Text(text = "Cancel",
+                    modifier = modifier)
+            }
             Button(
                 modifier = Modifier.weight(1f).fillMaxHeight(),
-                colors = ButtonDefaults.buttonColors(Green),
+                colors = ButtonDefaults.buttonColors(Green2),
                 contentPadding = PaddingValues(0.dp),
                 onClick = {}
-            ) {}
+            ) {
+                Text(text = "End match",
+                    modifier = modifier)
+            }
         }
     }
 }
--- app/src/main/java/com/example/simon/ui/theme/Color.kt
+++ app/src/main/java/com/example/simon/ui/theme/Color.kt
@@ -9,6 +9,10 @@ val Blue = Color(0xFF0000FF)
 val Magenta = Color(0xFFFF00FF)
 val Yellow = Color(0xFFFFFF00)
 val Cyan = Color(0xFF00FFFF)
+val Red2 = Color(0xCCCC0000)
+val Green2 = Color(0xCC00CC00)
+
+
 
 /*
 val BuRed = ButtonColors(Red, Red, Red, Red)
