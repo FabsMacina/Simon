21504fa639fe7d2ae9ee108beb0a088c95351793
FabsMacina <fabioceretti04@gmail.com>
2026-04-20 17:45:10 +0200

    Added orientation management and fixed the layout

--- app/src/main/java/com/example/simon/MainActivity.kt
+++ app/src/main/java/com/example/simon/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.example.simon
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.ScrollState
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -23,14 +22,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -63,11 +62,18 @@ class MainActivity: ComponentActivity() {
 fun MainScreen(modifier: Modifier = Modifier){
     var buttonCounter by rememberSaveable { mutableStateOf("") }
     val scrollState = rememberScrollState()
+    val orientation = LocalConfiguration.current.orientation
+
     ConstraintLayout(modifier = modifier) {
         val (c1, c2, r1, r2) = createRefs()
-        Row(modifier = Modifier.fillMaxHeight(0.05f).constrainAs(r1) {
+        Row(modifier = if(orientation == Configuration.ORIENTATION_LANDSCAPE){
+            Modifier.fillMaxHeight(0.1f)
+        }else{
+            Modifier.fillMaxHeight(0.05f)
+        }.fillMaxWidth().constrainAs(r1) {
             top.linkTo(parent.top)
             bottom.linkTo(c1.top)
+            start.linkTo(parent.start)
             end.linkTo(parent.end)
         }, horizontalArrangement = Arrangement.End){
             Button(
@@ -83,20 +89,33 @@ fun MainScreen(modifier: Modifier = Modifier){
                 )
             }
         }
-        Column(modifier = Modifier.fillMaxHeight(0.5f).constrainAs(c1) {
+        Column(modifier = if(orientation == Configuration.ORIENTATION_LANDSCAPE){
+            Modifier.fillMaxHeight(0.95f).fillMaxWidth(0.5f)
+            }else{
+            Modifier.fillMaxHeight(0.5f).fillMaxWidth()
+        }
+            .constrainAs(c1) {
             top.linkTo(r1.bottom, margin = 0.dp)
+            start.linkTo(parent.start)
+            if(orientation == Configuration.ORIENTATION_LANDSCAPE){
+                bottom.linkTo(parent.bottom)
+                end.linkTo(c2.start)
+            }else{
+                bottom.linkTo(c2.top)
+                end.linkTo(parent.end)
+            }
         },
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.spacedBy(0.dp)) {
             Row(modifier = Modifier.weight(1f).fillMaxHeight(), verticalAlignment = Alignment.CenterVertically) {
                 Button(
-                    modifier = modifier.weight(1f).fillMaxHeight().padding(top = 0.dp),
+                    modifier = Modifier.weight(1f).fillMaxHeight().padding(top = 0.dp),
                     colors = ButtonDefaults.buttonColors(Red),
                     contentPadding = PaddingValues(0.dp),
                     onClick = { buttonCounter += "R, " }
                 ) {}
                 Button(
-                    modifier = modifier.weight(1f).fillMaxHeight().padding(top = 0.dp),
+                    modifier = Modifier.weight(1f).fillMaxHeight().padding(top = 0.dp),
                     colors = ButtonDefaults.buttonColors(Green),
                     contentPadding = PaddingValues(0.dp),
                     onClick = { buttonCounter += "G, " }
@@ -104,13 +123,13 @@ fun MainScreen(modifier: Modifier = Modifier){
             }
             Row(modifier = Modifier.weight(1f), verticalAlignment = Alignment.CenterVertically) {
                 Button(
-                    modifier = modifier.weight(1f).fillMaxHeight(),
+                    modifier = Modifier.weight(1f).fillMaxHeight(),
                     colors = ButtonDefaults.buttonColors(Blue),
                     contentPadding = PaddingValues(0.dp),
                     onClick = { buttonCounter += "B, " }
                 ) {}
                 Button(
-                    modifier = modifier.weight(1f).fillMaxHeight(),
+                    modifier = Modifier.weight(1f).fillMaxHeight(),
                     colors = ButtonDefaults.buttonColors(Magenta),
                     contentPadding = PaddingValues(0.dp),
                     onClick = { buttonCounter += "M, " }
@@ -118,22 +137,34 @@ fun MainScreen(modifier: Modifier = Modifier){
             }
             Row(modifier = Modifier.weight(1f), verticalAlignment = Alignment.CenterVertically) {
                 Button(
-                    modifier = modifier.weight(1f).fillMaxHeight(),
+                    modifier = Modifier.weight(1f).fillMaxHeight(),
                     colors = ButtonDefaults.buttonColors(Yellow),
                     contentPadding = PaddingValues(0.dp),
                     onClick = { buttonCounter += "Y, " }
                 ) {}
                 Button(
-                    modifier = modifier.weight(1f).fillMaxHeight(),
+                    modifier = Modifier.weight(1f).fillMaxHeight(),
                     colors = ButtonDefaults.buttonColors(Cyan),
                     contentPadding = PaddingValues(0.dp),
                     onClick = { buttonCounter += "C, " }
                 ) {}
             }
         }
-        Column(modifier = Modifier.padding(0.dp).fillMaxHeight(0.2f).constrainAs(c2){
-            top.linkTo(c1.bottom)
-            }  ){
+        Column(modifier = if(orientation == Configuration.ORIENTATION_LANDSCAPE){
+            Modifier.fillMaxHeight(0.7f).fillMaxWidth(0.5f)
+        }else{
+            Modifier.fillMaxHeight(0.35f).fillMaxWidth()
+        }.padding(0.dp).constrainAs(c2){
+            end.linkTo(parent.end)
+            bottom.linkTo(r2.top)
+            if(orientation == Configuration.ORIENTATION_LANDSCAPE){
+                top.linkTo(r1.bottom)
+                start.linkTo(c1.end)
+            }else{
+                top.linkTo(c1.bottom)
+                start.linkTo(parent.start)
+            }
+        }  ){
             Text(
                 text = buttonCounter,
                 modifier = modifier.verticalScroll(scrollState),
@@ -141,25 +172,36 @@ fun MainScreen(modifier: Modifier = Modifier){
                 fontWeight = FontWeight.Medium
             )
         }
-        Row(modifier = Modifier.padding(0.dp).fillMaxHeight(0.1f).constrainAs(r2){
+        Row(modifier = if(orientation == Configuration.ORIENTATION_LANDSCAPE){
+            Modifier.fillMaxHeight(0.2f).fillMaxWidth(0.5f)
+        }else{
+            Modifier.fillMaxHeight(0.1f).fillMaxWidth()
+        }.padding(0.dp).constrainAs(r2){
             top.linkTo(c2.bottom)
+            bottom.linkTo(parent.bottom)
+            end.linkTo(parent.end)
+            if(orientation == Configuration.ORIENTATION_LANDSCAPE){
+                start.linkTo(c1.end)
+            }else{
+                start.linkTo(parent.start)
+            }
         } ){
             Button(
                 modifier = Modifier.weight(1f).fillMaxHeight(),
                 colors = ButtonDefaults.buttonColors(Red2),
                 contentPadding = PaddingValues(0.dp),
-                onClick = {}
+                onClick = {buttonCounter = ""}
             ) {
-                Text(text = "Cancel",
+                Text(text = stringResource(R.string.canc),
                     modifier = modifier)
             }
             Button(
                 modifier = Modifier.weight(1f).fillMaxHeight(),
                 colors = ButtonDefaults.buttonColors(Green2),
                 contentPadding = PaddingValues(0.dp),
-                onClick = {}
+                onClick = {buttonCounter = ""}
             ) {
-                Text(text = "End match",
+                Text(text = stringResource(R.string.end),
                     modifier = modifier)
             }
         }
