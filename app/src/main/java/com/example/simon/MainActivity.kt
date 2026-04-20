8f9ee5dd5ee12f22508e9ca292af36e1c9dc736d
FabsMacina <fabioceretti04@gmail.com>
2026-04-20 11:03:52 +0200

    Added a sequence string and working onClick for buttons

--- app/src/main/java/com/example/simon/MainActivity.kt
+++ app/src/main/java/com/example/simon/MainActivity.kt
@@ -4,27 +4,34 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
-import com.example.simon.ui.theme.BuBlue
-import com.example.simon.ui.theme.BuCyan
-import com.example.simon.ui.theme.BuGreen
-import com.example.simon.ui.theme.BuMagenta
-import com.example.simon.ui.theme.BuRed
-import com.example.simon.ui.theme.BuYellow
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.simon.ui.theme.Blue
+import com.example.simon.ui.theme.Cyan
+import com.example.simon.ui.theme.Green
+import com.example.simon.ui.theme.Magenta
+import com.example.simon.ui.theme.Red
+import com.example.simon.ui.theme.Yellow
 import com.example.simon.ui.theme.SimonTheme
 
 
@@ -44,49 +51,57 @@ class MainActivity: ComponentActivity() {
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier){
+    var buttonCounter by rememberSaveable { mutableStateOf("") }
     ConstraintLayout(modifier = modifier) {
-        val (c1) = createRefs()
+        val (c1, r1) = createRefs()
         Column(modifier = modifier.constrainAs(c1) {},
             horizontalAlignment = Alignment.CenterHorizontally) {
             Row(modifier = Modifier, verticalAlignment = Alignment.CenterVertically) {
                 Button(
                     modifier = modifier.weight(1f),
-                    colors = BuRed,
-                    onClick = {}
+                    colors = ButtonDefaults.buttonColors(Red),
+                    onClick = { buttonCounter += "R, " }
                 ) {}
                 Button(
                     modifier = modifier.weight(1f),
-                    colors = BuGreen,
-                    onClick = {}
-                ) {}
-            }
-            Row(modifier = Modifier, verticalAlignment = Alignment.CenterVertically) {
-                Button(
-                    modifier = modifier.weight(1f),
-                    colors = BuBlue,
-                    onClick = {}
-                ) {}
-                Button(
-                    modifier = modifier.weight(1f),
-                    colors = BuMagenta,
-                    onClick = {}
+                    colors = ButtonDefaults.buttonColors(Green),
+                    onClick = { buttonCounter += "G, " }
                 ) {}
             }
             Row(modifier = Modifier, verticalAlignment = Alignment.CenterVertically) {
                 Button(
                     modifier = modifier.weight(1f),
-                    colors = BuYellow,
-                    onClick = {}
+                    colors = ButtonDefaults.buttonColors(Blue),
+                    onClick = { buttonCounter += "B, " }
                 ) {}
                 Button(
                     modifier = modifier.weight(1f),
-                    colors = BuCyan,
-                    onClick = {}
+                    colors = ButtonDefaults.buttonColors(Magenta),
+                    onClick = { buttonCounter += "M, " }
+                ) {}
+            }
+            Row(modifier = Modifier, verticalAlignment = Alignment.CenterVertically) {
+                Button(
+                    modifier = modifier.weight(1f),
+                    colors = ButtonDefaults.buttonColors(Yellow),
+                    onClick = { buttonCounter += "Y, " }
+                ) {}
+                Button(
+                    modifier = modifier.weight(1f),
+                    colors = ButtonDefaults.buttonColors(Cyan),
+                    onClick = { buttonCounter += "C, " }
                 ) {}
             }
         }
-        Row(modifier = modifier){
-
+        Row(modifier = modifier.padding(10.dp).constrainAs(r1){
+            top.linkTo(c1.bottom)
+        }){
+            Text(
+                text = buttonCounter,
+                modifier = modifier,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Medium
+            )
         }
     }
 }
--- app/src/main/java/com/example/simon/ui/theme/Color.kt
+++ app/src/main/java/com/example/simon/ui/theme/Color.kt
@@ -3,16 +3,18 @@ package com.example.simon.ui.theme
 import androidx.compose.material3.ButtonColors
 import androidx.compose.ui.graphics.Color
 
-val Red = Color(0xFF0000)
-val Green = Color(0x00FF00)
-val Blue = Color(0x0000FF)
-val Magenta = Color(0xFF00FF)
-val Yellow = Color(0xFFFF00)
-val Cyan = Color(0x00FFFF)
+val Red = Color(0xFFFF0000)
+val Green = Color(0xFF00FF00)
+val Blue = Color(0xFF0000FF)
+val Magenta = Color(0xFFFF00FF)
+val Yellow = Color(0xFFFFFF00)
+val Cyan = Color(0xFF00FFFF)
 
+/*
 val BuRed = ButtonColors(Red, Red, Red, Red)
 val BuGreen = ButtonColors(Green, Green, Green, Green)
 val BuBlue = ButtonColors(Blue, Blue, Blue, Blue)
 val BuMagenta = ButtonColors(Magenta, Magenta, Magenta, Magenta)
 val BuYellow = ButtonColors(Yellow, Yellow, Yellow, Yellow)
 val BuCyan = ButtonColors(Cyan, Cyan, Cyan, Cyan)
+*/
